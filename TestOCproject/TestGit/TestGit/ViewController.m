f9e260c0a8c38fdefd55c22a0fc53a3c3db65128
马鸣 <m765462460@126.com>
2017-11-22 21:29:19 +0800

    下属二第一次操作

--- TestOCproject/TestGit/TestGit/ViewController.m
+++ TestOCproject/TestGit/TestGit/ViewController.m
@@ -17,9 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"下属一第一次操作");
-    NSLog(@"创建dev分支");
-    NSLog(@"创建dev分支");
+    NSLog(@"下属二第一次操作");
+    
 }
 
 
